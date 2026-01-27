0971e88e6444c429b241156c08f63462eb6e2f86
Najib Tri Setiawan <08.najibtrisetiawan@gmail.com>
2026-01-27 18:02:01 +0700

    day 1 - Edit

--- string.go
+++ string.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Najib")
-	fmt.Println("Najib Tri")
-	fmt.Println("Najib Tri Setiawan")
+	fmt.Println("Najib Orang")
+	fmt.Println("Najib Orang Baik")
 
 	fmt.Println(len("Najib"))
-	fmt.Println("Najib Tri"[0])
-	fmt.Println("Najib Tri Setiawan"[7])
+	fmt.Println("Najib Orang"[0])
+	fmt.Println("Najib Orang Baik"[7])
 }
