7be63c4a606c316e834090e159e0842c5b2b6ab2
Najib Tri Setiawan <08.najibtrisetiawan@gmail.com>
2026-01-27 15:45:52 +0700

    day 1

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Najib")
+	fmt.Println("Najib Tri")
+	fmt.Println("Najib Tri Setiawan")
+
+	fmt.Println(len("Najib"))
+	fmt.Println("Najib Tri"[0])
+	fmt.Println("Najib Tri Setiawan"[7])
+}
